45ee8d9d4b6d7dd301a73a21f39b0e80a1f3732e
Pedro Eduardo <eduardototi@gmail.com>
2020-11-19 17:32:50 -0300

    29 day challenge 2 - created routes and controllers

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,49 @@
+class CocktailsController < ApplicationController
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktails_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail), notice: 'Cocktail was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def update
+    @cocktail = Cocktail.find(params[:id])
+    if @cocktail.update(cocktails_params)
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+    redirect_to cocktails_path, notice: 'Cocktail was successfully destroyed.'
+  end
+
+  private
+
+  def cocktails_params
+    params.require(:cocktail).permit(:name)
+  end
+
+end
---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,33 @@
+class DosesController < ApplicationController
+
+  def create
+
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktails_path
+    else
+      render :new
+    end
+  end
+
+  def new
+    @ingredients = Ingredient.all
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktails_path(@dose.cocktail)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+
+end
