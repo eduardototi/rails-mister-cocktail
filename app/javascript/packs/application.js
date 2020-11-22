ed64ec05c704a8254a6268b9f6040fcd948aa08c
Pedro Eduardo <eduardototi@gmail.com>
2020-11-22 18:45:46 -0300

    cocktails challenge - css

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,9 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+import $ from 'jquery';
 import 'bootstrap';
-
+import 'slick-carousel';
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -16,3 +17,10 @@ import 'bootstrap';
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+const initSlick = () => {
+  $('.your-class').slick()
+}
+
+$(document).on('turbolinks:load', function() {
+  initSlick();
+})
